2737fa3b63c48b4a691ce3461accda8df257c4f0
Mohammed <mohammeduwais.28ad@licet.ac.in>
2026-09-07 12:31:41 +0530

    Bump Service Worker cache to v4 and add script cache buster in index.html to force cache purge on client browsers

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CampusPulse Universal PWA Service Worker with Offline Caching & FCM Closed-Browser Push Engine
-const CACHE_NAME = 'campuspulse-pwa-v3';
+const CACHE_NAME = 'campuspulse-pwa-v4';
 
 const ASSETS_TO_CACHE = [
   './',
